7fdebf1b634f0c12e556ec53edf6f0bdf4f8b3ba
anasali2193 <anasali2193@gmail.com>
2020-05-19 23:28:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,23 @@ db.create_all()
 #controller
 @app.route('/todos/create',methods=['POST'])
 def todo_create():
-    description = request.get_json()['description']
-    todo =Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description' :todo.description
-    })
+    error= False
+    body={}
+    try:
+        description = request.get_json()['description']
+        todo =Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description']=todo.description
+       
+    except:
+        error=True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+        if not error:
+            return jsonify(body)
 
 @app.route('/')
 def index():
